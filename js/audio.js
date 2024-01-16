f70e9b62f3780beded1d1b0bfad641580cbb9f16
Yuri <134183050+Yunnashi@users.noreply.github.com>
2024-01-16 21:36:18 +0900

    Add files via upload

---
+++ js/audio.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+
+function sound()
+{
+	// [ID:sound-file]の音声ファイルを再生[play()]する
+	document.getElementById( 'sound-file' ).play() ;
+}
+function sound2()
+{
+	// [ID:sound-file]の音声ファイルを再生[play()]する
+	document.getElementById( 'sound-file2' ).play() ;
+}
+function sound3()
+{
+	// [ID:sound-file]の音声ファイルを再生[play()]する
+	document.getElementById( 'sound-file3' ).play() ;
+}
